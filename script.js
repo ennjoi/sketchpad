00d6d508d85868cee2a5c04b82099a6135523f6d
ennjoi <paulo.e.aquino@gmail.com>
2015-09-14 20:10:54 +0900

    updates

--- script.js
+++ script.js
@@ -2,4 +2,14 @@ $(document).ready(function() {
 	$('#addblock').click(function() {
 		$('#container').append('<div class="red"></div>');
 	});
+	$('#removeblock').click(function() {
+		$(this).closest('red').remove();
+	});
+	$('div').mouseover(
+		function() {
+			$(this).addClass('active');
+		}, 
+		function() {
+			$(this).removeClass('active');
+		});
 });
